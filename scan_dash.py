c6d342a63f724b2f752ebafae803e26c34b634cd
whywbhydyq <aren.ymir@gmail.com>
2026-04-24 01:58:29 +0800

    fix: 补充tabRef声明和滚动逻辑

---
+++ scan_dash.py
@@ -0,0 +1,5 @@
+﻿with open('src/pages/DashboardPage.tsx', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+for i, line in enumerate(lines, 1):
+    if 'tabRef' in line or 'scrollIntoView' in line:
+        print(f'L{i}: {line}', end='')
