92d34f494d83e80b9a9ac561af3b00a8f98b4e47
whywbhydyq <aren.ymir@gmail.com>
2026-04-26 21:32:13 +0800

    add: sitemap.xml + robots.txt

---
+++ create_sitemap.ps1
@@ -0,0 +1,29 @@
+﻿$domain = "https://paperfixes.com"
+
+$sitemap = @"
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>$domain/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>$domain/pricing</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>$domain/dashboard</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.6</priority>
+  </url>
+</urlset>
+"@
+
+[System.IO.File]::WriteAllText(
+    (Join-Path $PWD "public\sitemap.xml"),
+    $sitemap,
+    [System.Text.Encoding]::UTF8
+)
+Write-Host "[OK] public/sitemap.xml created"
